a0c89de19e6f8a0efd9ce3a78194074dbd5164c8
Joseph Gallagher <uhhhmmm@gmail.com>
2015-05-01 23:52:59 -0400

    Updated views.py

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from serializers import *
 
 
+
 class EventList(generics.ListAPIView):
     serializer_class = EventSerializer
     queryset = Event.objects.all()
