68c91af50ed6cbb28c844247e09d83f41eb1c48e
David Harrison Smith <exmokie@gmail.com>
2015-05-04 11:22:50 -0600

    edits

--- main/models.py
+++ main/models.py
@@ -1,21 +1,19 @@
 from django.db import models
 
-# Create your models here.
 
 class Tag (models.Model):
-    name=models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+
     def __str__(self):
         return self.name
 
 
 class Event (models.Model):
-    location=models.CharField(max_length=200)
-    picture=models.ImageField(upload_to='photos', blank=False, null=False)
-    caption=models.CharField(max_length=300)
-    date_time=models.CharField(max_length=30)
+    location = models.CharField(max_length=200)
+    picture = models.ImageField(upload_to='photos', blank=False, null=False)
+    caption = models.CharField(max_length=300)
+    date_time = models.CharField(max_length=30)
     tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.caption[:20]
-
-
