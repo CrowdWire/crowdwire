c4c6eb42ddf4cdf8a3d4c46c0f8e8d2113c6bd8c
Joseph Gallagher <uhhhmmm@gmail.com>
2015-05-06 13:30:08 -0400

    committing changes before pulling from github dev

--- frontend/app/add-event/addEvent.js
+++ frontend/app/add-event/addEvent.js
@@ -57,40 +57,5 @@ angular.module('myApp.addEvent', ['ngRoute'])
                 alert("There was a problem submitting the event...")
             });
         };
-         //Initialize the map, center it and set the zoom level.
-
-        var myLatlng = new google.maps.LatLng(45.00000,45.00000);
-        var mapOptions = {
-            zoom: 7,
-            center: myLatlng
-        };
-        var map = new google.maps.Map(document.getElementById('map-canvas'),
-                mapOptions);
-
-
-        google.maps.event.addListener(map, 'click', function (e) {
-            placeMarker(e.latLng, map);
-        });
-
-
-    //Create the pin marker itself, and set its characteristics
-    function placeMarker(position, map) {
-        var marker = new google.maps.Marker({
-            position: position,
-            map: map,
-            draggable:true
-        });
-
-        //Add an event listener on the pin marker.  When clicked, spit out the lat and lng data
-        google.maps.event.addListener(marker, "click", function (event) {
-            var latitude = event.latLng.lat();
-            var longitude = event.latLng.lng();
-            alert(latitude + ', ' + longitude);
-        });
-        map.panTo(position);
-    }
-
-
-    google.maps.event.addDomListener(window, 'load', initialize);
 
     }]);
